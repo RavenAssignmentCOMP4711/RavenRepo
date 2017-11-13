575144d1faa8ab3fa17d3b81b8aa4f58142eb668
ryanhhtan <ryanhhtan@gmail.com>
2017-11-12 19:26:23 -0800

    added fleets (collection) unit test
    Signed-off-by: ryanhhtan <ryanhhtan@gmail.com>

--- application/models/Fleets.php
+++ application/models/Fleets.php
@@ -20,4 +20,16 @@ class Fleets extends CSV_Model
         );
         return $config;
     }
+
+    function add($record) {
+        $record = (array) $record;
+        if (strtolower(substr($record['id'], 0, 1)) != 'r')
+            return false;
+        $airplane = json_decode(file_get_contents('http://wacky.jlparry.com/info/airplanes/' . $record['plane_id']));
+
+        if ($airplane == null)
+            return false;
+
+        parent::add($record);
+    }
 }
