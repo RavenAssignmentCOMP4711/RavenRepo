aa08a37c7fbd2e77d3a42b57a401488284579365
Harshita Sharma <hsharma15@my.bcit.ca>
2017-11-12 17:38:09 -0800

    Validation stuff
    Signed-off-by: Harshita Sharma <hsharma15@my.bcit.ca>

--- application/models/Fleet.php
+++ application/models/Fleet.php
@@ -1,5 +1,7 @@
 <?php
+
 class Fleet extends Entity {
+
     protected $id;
     protected $plane_id;
     protected $manufacturer;
@@ -11,8 +13,7 @@ class Fleet extends Entity {
     protected $takeoff;
     protected $hourly;
 
-    public function get($id = null) 
-    {
+    public function get($id = null) {
 
         if ($id == null)
             return $this;
@@ -20,70 +21,74 @@ class Fleet extends Entity {
         $record = $this->loadCollectionModel('fleets')->get($id);
 
         if ($record == null)
-            return $this; 
+            return $this;
 
 
-        $plane = json_decode(file_get_contents('http://wacky.jlparry.com/info/airplanes/'. $record->plane_id));
+        $plane = json_decode(file_get_contents('http://wacky.jlparry.com/info/airplanes/' . $record->plane_id));
         if ($plane == null)
             return $this;
 
-        foreach ($plane as $key =>$value)
-        {
+        foreach ($plane as $key => $value) {
             $this->$key = $value;
         }
 
         return $this;
     }
 
-    public function setId($id)
-    {
+    public function setId($id) {
         // Fleet id cannot be modified, delete and add a new one instead
         return false;
     }
 
-    public function setPlaneid($plane_id)
-    {
+    public function setPlaneid($plane_id) {
         // validate the parament before accepting it.
         // if no validation needed, just delete this function
+        $plane = json_decode(file_get_contents('http://wacky.jlparry.com/info/airplanes/' . $plane_id));
+        if ($plane == null)
+            return false;
         $this->plane_id = $plane_id;
+
+        // also update other properties 
+        foreach ($plane as $key => $value) {
+            $this->$key = $value;
+        }
+
+        return true;
     }
 
-    public function setModel($model) 
-    {
-        $this->model = $model;
+    public function setModel($model) {
+        // this property cannot be directly changed.
+        return false;
     }
 
-    public function setPrice($price) 
-    {
-        // validate the parament before accepting it.
-        // if no validation needed, just delete this function
-        $this->price = $price;
+    public function setPrice($price) {
+        // this property cannot be directly changed.
+        return false;
     }
 
-    public function setSeats($seats) 
-    {
-        // validate the parament before accepting it.
-        // if no validation needed, just delete this function
-        $this->seats = $seats;
+    public function setSeats($seats) {
+        // this property cannot be directly changed.
+        return false;
     }
 
-    public function setReach($reach) 
-    {
-        // validate the parament before accepting it.
-        // if no validation needed, just delete this function
-        $this->reach = $reach;
+    public function setReach($reach) {
+        // this property cannot be directly changed.
+        return false;
     }
 
-    public function setCruise($cruise) 
-    {
-        // validate the parament before accepting it.
-        // if no validation needed, just delete this function
-        $this->cruise = $cruise;
+    public function setCruise($cruise) {
+        // this property cannot be directly changed.
+        return false;
     }
-    public function setTakeoff($takeoff) 
-    {
+
+    public function setTakeoff($takeoff) {
+        // this property cannot be directly changed.
+        return false;
     }
-    public function setHourly($hourly) 
-    {
+
+    public function setHourly($hourly) {
+        // this property cannot be directly changed.
+        return false;
     }
+
 }
